ea4453d08a3e18a568518aa6272ece9cc31161f9
Shawn Lee <shawn.l@samsung.com>
2017-01-21 13:16:57 -0800

    Radix Sort implementation

--- sorting/radix_sort.cc
+++ sorting/radix_sort.cc
@@ -7,12 +7,12 @@ void PrintArray(const int* arr, int n) {
   std::cout << "}" << std::endl;
 }
 
-void CountingSort(int* arr, int n, int k) {
+void CountingSort(int* arr, int n, int k, int exp) {
   int counter[k] = {};
   for (int i = 0; i < k; i++)
     counter[i] = 0;
   for (int i = 0; i < n; i++)
-    counter[arr[i]]++;
+    counter[ (arr[i]/exp)%k ]++;
   for (int i = 1; i < k; i++)
     counter[i] += counter[i-1];
   
@@ -20,22 +20,36 @@ void CountingSort(int* arr, int n, int k) {
   for (int i = 0; i < n; i++)
     temp[i] = arr[i];
   for (int i = n - 1; i >= 0; i--) {
-    int index = --counter[temp[i]];
+    int index = --counter[ (temp[i]/exp)%k ];
     arr[index] = temp[i];
   }
+}
+
+void RadixSort(int* arr, int n) {
+  // find max value
+  int max = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] > max)
+      max = arr[i];
+  }
+  
+  // e represents each digit as the power of 10
+  const int base = 10;
+  for (int e = 1; max/e > 0; e *= base)
+    CountingSort(arr, n, base, e);
+  
 }
 
 int main() {
   int n = 5;
   int* arr = new int[n];
-  arr[0] = 3;
-  arr[1] = 5;
-  arr[2] = 1;
-  arr[3] = 9;
-  arr[4] = 2;
+  arr[0] = 310;
+  arr[1] = 5074;
+  arr[2] = 10;
+  arr[3] = 90020;
+  arr[4] = 258;
   
-  int k = 10;
   PrintArray(arr, n);
-  CountingSort(arr, n, k);
+  RadixSort(arr, n);
   PrintArray(arr, n);
 }
